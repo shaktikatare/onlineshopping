e0b67f3ec431086e9f26ca96af9ba9951c16abfe
Shakti <shakti@grepruby.com>
2013-10-09 16:37:10 +0530

    display orders to user and admin

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,9 +8,25 @@ class OrdersController < ApplicationController
     @order.user_id=current_user.id
     @order.order_status = false
     if @order.save
-      flash[:notice] = "order is created successfully"
+      @cart = Cart.where(:user_id => current_user.id)
+      @cart.each do |c|
+        @orderdetails=Orderdetail.create(:order_id => @order.id, :product_id => c.product_id, :quantity => c.qty)
+      end
+      @cart.destroy_all  
+      flash[:notice] = "order is placed successfully"
+      redirect_to root_path
+      else
+      flash[:notice] = "order is not placed successfully"
       redirect_to root_path
     end
   end
- 
+  
+  def index
+    if current_user.is_admin?
+      @order = Order.all
+    else  
+      @order = Order.where(:user_id => current_user.id)
+    end  
+  end
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ActiveRecord::Base
   attr_accessible :city, :full_name, :order_status, :phone, :pin_code, :shipping_address, :state, :user_id
-  belongs_to :user, :foreign_key => 'user_id'
-  has_many :order_details
+  belongs_to :user
+  has_many :orderdetail
 end
