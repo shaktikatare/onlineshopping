7a718bb3c10e2058c42d86267c46acc705c87021
Shakti <shakti@grepruby.com>
2013-10-16 13:53:07 +0530

    fix message for devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,12 @@ Mydemo1::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  
+    # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
