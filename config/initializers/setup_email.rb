31086c3b750c2646fb73684b5d59929df76f664b
Shakti <shakti@grepruby.com>
2013-10-17 12:37:30 +0530

    set_up email

---
+++ config/initializers/setup_email.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.smtp_settings = {
+:address => "smtp.gmail.com",
+:port => 587,
+:domain => "mail.google.com",
+:user_name => "onlineshoppinggr@gmail.com",
+:password => "shakti1234567890",
+:authentication => :login,
+:enable_starttls_auto => true
+}
+ActionMailer::Base.default_url_options[:host] = "onlineshopping-of-grepruby.herokuapp.com"
